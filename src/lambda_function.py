6125466f02ec6c6eadf61bebd3d9ff3c14d4b67f
Ranu Singh <sranu2109@gmail.com>
2026-01-07 20:07:20 +0530

    update lambda commit

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! A New Update! ')
+        'body': json.dumps('Hello from Lambda! woohoo! ')
     }
